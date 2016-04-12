8a6c94a9b92bde784eb629d6d9cdaa42fc1dadf7
Angelo G. Del Regno <kholk11@gmail.com>
2016-04-12 13:08:12 +0200

    RADIO IRIS REVERT THE MODULE COMMIT --- QCOM FIXED EVERYTHING

--- drivers/media/radio/radio-iris-transport.c
+++ drivers/media/radio/radio-iris-transport.c
@@ -206,7 +206,6 @@ static void radio_hci_smd_deregister(void)
 	fmsmd_set = 0;
 }
 
-#ifdef MODULE
 static int radio_hci_smd_init(void)
 {
 	return radio_hci_smd_register_dev(&hs);
@@ -216,22 +215,6 @@ static void radio_hci_smd_exit(void)
 {
 	radio_hci_smd_deregister();
 }
-
-int hci_fm_smd_register(void) { return 0; }
-void hci_fm_smd_deregister(void) { }
-
-#else
-
-int hci_fm_smd_register(void)
-{
-	return radio_hci_smd_register_dev(&hs);
-}
-
-void hci_fm_smd_deregister(void)
-{
-	radio_hci_smd_deregister();
-}
-#endif
 
 static int hcismd_fm_set_enable(const char *val, struct kernel_param *kp)
 {
