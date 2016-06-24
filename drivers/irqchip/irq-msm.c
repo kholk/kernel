e3b45b65380994a8983eb200632dd93e49b03034
Angelo G. Del Regno <kholk11@gmail.com>
2016-06-24 14:14:32 +0200

    pinctrl: Implement MSM8994 pinctrl driver
    Add pinctrl-msm8994.c, based on msm8x74 and add its dt label
    to irqchip to initialize irq-msm.

--- drivers/irqchip/irq-msm.c
+++ drivers/irqchip/irq-msm.c
@@ -68,5 +68,6 @@ IRQCHIP_DECLARE(gold_pinctrl, "qcom,msmgold-pinctrl", pinctrl_irq_dummy);
 IRQCHIP_DECLARE(8952_pinctrl, "qcom,msm8952-pinctrl", pinctrl_irq_dummy);
 IRQCHIP_DECLARE(8937_pinctrl, "qcom,msm8937-pinctrl", pinctrl_irq_dummy);
 IRQCHIP_DECLARE(8916_pinctrl, "qcom,msm8916-pinctrl", pinctrl_irq_dummy);
+IRQCHIP_DECLARE(8994_pinctrl, "qcom,msm8994-pinctrl", pinctrl_irq_dummy);
 IRQCHIP_DECLARE(titanium_pinctrl, "qcom,msmtitanium-pinctrl",
 						pinctrl_irq_dummy);
