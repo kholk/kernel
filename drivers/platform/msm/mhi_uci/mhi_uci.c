b5924d280eced0851e423b56f6480ef615acb036
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-01-04 16:51:53 +0100

    soc: qcom: stop spam when IPC_LOGGING is disabled
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- drivers/platform/msm/mhi_uci/mhi_uci.c
+++ drivers/platform/msm/mhi_uci/mhi_uci.c
@@ -1494,6 +1494,7 @@ static struct platform_driver mhi_uci_driver = {
 
 static int mhi_uci_init(void)
 {
+#ifdef CONFIG_IPC_LOGGING
 	mhi_uci_drv_ctxt.mhi_uci_ipc_log =
 		ipc_log_context_create(MHI_UCI_IPC_LOG_PAGES,
 				       "mhi-uci",
@@ -1503,7 +1504,7 @@ static int mhi_uci_init(void)
 			UCI_DBG_WARNING,
 			"Failed to create IPC logging context");
 	}
-
+#endif
 	mhi_uci_drv_ctxt.mhi_uci_class =
 		class_create(THIS_MODULE, DEVICE_NAME);
 
